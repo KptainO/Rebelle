c222604fac3871e3f2910a64639177bedc1f6213
KptainO <kptaino@gmail.com>
2013-11-11 23:39:30 +0100

    [project] export headers into Rebelle.h

---
+++ Rebelle/Rebelle.h
@@ -0,0 +1,13 @@
+//
+// This file is part of Rebelle
+//  
+//  Created by JC on 11/11/13.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
+//
+
+// Umbrella file
+
+#import "RBErrorException.h"
+#import "RBPromise.h"
+#import "RBThenable.h"
