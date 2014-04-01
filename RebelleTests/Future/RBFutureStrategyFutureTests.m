4c68c08fdef7d02502a1e1f6f9d4221312de7796
KptainO <kptaino@gmail.com>
2014-04-02 00:47:34 +0200

    [tests] added new tests with RBFutureArray

--- RebelleTests/Future/RBFutureStrategyFutureTests.m
+++ RebelleTests/Future/RBFutureStrategyFutureTests.m
@@ -10,20 +10,25 @@
 
 #import "RBFutureStrategyFuture.h"
 #import "RBFuture.h"
+#import "RBFutureArray.h"
 
 SPEC_BEGIN(RBFutureStrategyFutureTests)
 
 describe(@"test", ^{
    __block RBFutureStrategyFuture *strategy;
-   __block RBFuture *future;
    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
 
    beforeEach(^{
       strategy = [RBFutureStrategyFuture new];
-      future = [RBFuture mock];
    });
 
-   describe(@"compute", ^{
+   describe(@"compute single RBFuture", ^{
+      __block RBFuture *future;
+
+      beforeEach(^{
+         future = [RBFuture mock];
+      });
+
       it(@"with RBFuture pending, then fulfilled", ^{
          [future stub:@selector(state) andReturn:theValue(RBFutureStatePending)];
          [future stub:@selector(result) andReturn:@"Hello World"];
@@ -59,6 +64,57 @@ describe(@"test", ^{
                             withArguments:@"RBComputationDoneNotification", strategy, @{ @"result": [NSNull null] }];
 
          [strategy compute:future];
+      });
+   });
+
+   describe(@"compute array of futures", ^{
+      __block RBFuture *f1;
+      __block RBFuture *f2;
+      __block KWCaptureSpy *userInfo;
+
+      beforeEach(^{
+         f1 = [RBFuture mock];
+         f2 = [RBFuture mock];
+         userInfo = [notificationCenter captureArgument:@selector(postNotificationName:object:userInfo:) atIndex:2];
+      });
+
+      it(@"fulfilled", ^{
+         [f1 stub:@selector(state) andReturn:theValue(RBFutureStateFulfilled)];
+         [f1 stub:@selector(result) andReturn:@"Hello"];
+         [f2 stub:@selector(state) andReturn:theValue(RBFutureStateFulfilled)];
+         [f2 stub:@selector(result) andReturn:@"World"];
+
+         [strategy compute:[RBFutureArray arrayWithFutures:f1,f2,nil]];
+
+         [[userInfo.argument should] equal:@{ @"result": @[@"Hello", @"World"] }];
+      });
+
+      it(@"with one rejected", ^{
+         [f1 stub:@selector(state) andReturn:theValue(RBFutureStateFulfilled)];
+         [f1 stub:@selector(result) andReturn:@"Hello"];
+         [f2 stub:@selector(state) andReturn:theValue(RBFutureStateRejected)];
+         [f2 stub:@selector(result) andReturn:@"NSException"];
+
+         [strategy compute:[RBFutureArray arrayWithFutures:f1,f2,nil]];
+
+         [[userInfo.argument should] equal:@{ @"result": @"NSException" }];
+      });
+
+      it(@"with one pending", ^{
+         [f1 stub:@selector(state) andReturn:theValue(RBFutureStateFulfilled)];
+         [f1 stub:@selector(result) andReturn:@"Hello"];
+         [f2 stub:@selector(state) andReturn:theValue(RBFutureStatePending)];
+         [f2 stub:@selector(result) andReturn:@"World"];
+
+         [strategy compute:[RBFutureArray arrayWithFutures:f1,f2,nil]];
+
+         [f2 stub:@selector(state) andReturn:theValue(RBFutureStateFulfilled)];
+         [strategy observeValueForKeyPath:RBFuturePropertyState
+                                 ofObject:f2
+                                   change:nil
+                                  context:(__bridge void *)(RBFuturePropertyState)];
+
+         [[userInfo.argument should] equal:@{ @"result": @[@"Hello", @"World"] }];
       });
    });
 });
